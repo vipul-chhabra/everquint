564772ee940a986c2a32d57175fe2b7aeab7fe00
unknown <chhabravipul21@gmail.com>
2026-08-11 13:48:18 +0530

    updated code

--- water tank/script.js
+++ water tank/script.js
@@ -20,10 +20,10 @@ const compute=()=>{
       inputArr[i] = Number(inputArr[i]);
       if(inputArr[i]>max) max=inputArr[i];
   }
-  const outVis = outVisArr(inputArr);
+  const outVis = waterCalculation(inputArr);
 
-  document.getElementsByClassName("inp-vis")[0].innerHTML = renderInput(inputArr,outVis, max, "inp-vis");
-  document.getElementsByClassName("output")[0].innerHTML = renderOutput(outVis, max, "out-vis");
+  document.getElementsByClassName("inp-vis")[0].innerHTML = renderInput(inputArr, outVis, max, "inp-vis");
+  document.getElementsByClassName("output")[0].innerHTML = renderOutput(inputArr, outVis, max, "out-vis");
 }
 
 const keyHandler=(e)=>{
@@ -33,33 +33,8 @@ const keyHandler=(e)=>{
     }
 }
 
-const outVisArr=(arr)=>{
-  let result=[];
-  let lastValueForForwardCase=0;
-  let lastValueForBackwardCase=0;
-  for (let fc = 0; fc < arr.length; fc++) {
-      let element = arr[fc];
-      if (element == 0) {
-          result.push(lastValueForForwardCase);
-      }else{
-          result.push(-1);
-          lastValueForForwardCase=element;
-      }
-  }
-  for (let  bc = arr.length - 1; bc >= 0; bc--) {
-      let element = arr[bc];
-      if (element == 0) {
-          result[bc] = result[bc] - lastValueForBackwardCase > 0 ? lastValueForBackwardCase : result[bc];
-      }else{
-          lastValueForBackwardCase=element;
-      }
-  }
 
-  return result;
-  
-}
-
-const renderInput = (inpArr, outVisArr, max, className)=>{
+const renderInput = (inpArr, newInpArr, max, className)=>{
   if(document.getElementsByTagName('head')[0].getElementsByTagName('style')[0]){
   style = document.getElementsByTagName('head')[0].getElementsByTagName('style')[0];
    }
@@ -67,21 +42,23 @@ const renderInput = (inpArr, outVisArr, max, className)=>{
       style = document.createElement('style');
       document.getElementsByTagName('head')[0].appendChild(style);
   }
-
   let inputTableStyle = "";
-  for(let i=0;i<outVisArr.length;i++) {
-      if(outVisArr[i]!=0 && outVisArr[i]!=-1){
-          inputTableStyle+=`.${className} td:nth-child(${i+1}) {background-color: DeepSkyBlue;}`;
-          for(let j=1;j<=max-outVisArr[i]+1;j++) {
-              inputTableStyle+=`.${className} tr:nth-child(${j}) td:nth-child(${i+1}) {background-color: white;}`;
-          }
-      }
-      if(outVisArr[i]==-1) {
-          inputTableStyle+=`.${className} td:nth-child(${i+1}) {background-color: yellow;}`;
-          for(let j=1;j<=max-inpArr[i]+1;j++) {
-              inputTableStyle+=`.${className} tr:nth-child(${j}) td:nth-child(${i+1}) {background-color: white;}`;
-          }
-      }
+
+  for(let i=0;i<inpArr.length;i++){
+    
+    for(let j=1;j<=max+1;j++){
+        
+        if(inpArr[i]>0 && inpArr[i]>=j){
+            inputTableStyle +=`.${className} tr:nth-child(${max-j+2}) td:nth-child(${i+1}) {background-color: yellow;}`;
+        }
+        else if(newInpArr[i]>0 && newInpArr[i] >= j - inpArr[i] ){
+            inputTableStyle +=`.${className} tr:nth-child(${max-j+2}) td:nth-child(${i+1}) {background-color: DeepSkyBlue;}`;
+        }
+        else {
+            inputTableStyle+=`.${className} tr:nth-child(${max-j+2}) td:nth-child(${i+1}) {background-color: white;}`;
+        }      
+    }
+    
   }
 
   style.appendChild(document.createTextNode(inputTableStyle));
@@ -102,24 +79,31 @@ function render(arr, max) {
   return render;
 }
 
-const renderOutput=(outVisArr, max, tableClassName)=>{
+const renderOutput=(inpArr, newInpArr, max, tableClassName)=>{
   style = document.getElementsByTagName('head')[0].getElementsByTagName('style')[0];
-  let units = totalUnits(outVisArr);
+  let units = totalUnits(newInpArr);
   resultBlock = `<h3>Output: ${units} ${units<=1?"Unit":"Units"}</h3>
                   <div class=${tableClassName}>`;
 
   let inputTableStyle = "";
-  for(let i=0;i<outVisArr.length;i++) {
-      if(outVisArr[i]!=0 && outVisArr[i]!=-1){
-          inputTableStyle+=`.${tableClassName} td:nth-child(${i+1}) {background-color: DeepSkyBlue;}`;
-          for(let j=1;j<=max-outVisArr[i]+1;j++) {
-              inputTableStyle+=`.${tableClassName} tr:nth-child(${j}) td:nth-child(${i+1}) {background-color: white;}`;
-          }
-      }
+  
+  for(let i=0;i<inpArr.length;i++){
+    for(let j=1;j<=max+1;j++){
+        
+        if(inpArr[i]>0 && inpArr[i]>=j){
+            inputTableStyle +=`.${tableClassName} tr:nth-child(${max-j+2}) td:nth-child(${i+1}) {background-color: white;}`;
+        }
+        else if(newInpArr[i]>0 && newInpArr[i] >= j - inpArr[i] ){
+            inputTableStyle +=`.${tableClassName} tr:nth-child(${max-j+2}) td:nth-child(${i+1}) {background-color: DeepSkyBlue;}`;
+        }
+        else {
+            inputTableStyle+=`.${tableClassName} tr:nth-child(${max-j+2}) td:nth-child(${i+1}) {background-color: white;}`;
+        }      
+    }
   }
 
   style.appendChild(document.createTextNode(inputTableStyle));
-  return resultBlock + render(outVisArr, max) + "</div>";
+  return resultBlock + render(newInpArr, max) + "</div>";
 }
 
 function totalUnits(outVisArr) {
@@ -127,4 +111,27 @@ function totalUnits(outVisArr) {
       if(ele==-1) return acc;
       return acc+ele;
   },0);
+}
+
+const waterCalculation=(inpArr)=> {
+  let newInpArr = new Array(inpArr.length);
+  let maxLeft = 0 ;
+  let maxRight = 0 ; 
+  let leftArr = 0 ; 
+  let rightArr = inpArr.length -1 ;
+  while(leftArr < rightArr) {
+      let newInputTableStyle = '';  
+      if(inpArr[leftArr] <= inpArr[rightArr]){
+        maxLeft = Math.max(maxLeft, inpArr[leftArr]) ;
+        let value  =  maxLeft - inpArr[leftArr];
+        newInpArr[leftArr] = value ; 
+        leftArr++ ;
+      }else{
+        maxRight = Math.max(maxRight, inpArr[rightArr]) ;
+        let rightValue = maxRight - inpArr[rightArr];
+        newInpArr[rightArr] = rightValue;
+        rightArr-- ;
+      }
+  }  
+  return newInpArr ;
 }
